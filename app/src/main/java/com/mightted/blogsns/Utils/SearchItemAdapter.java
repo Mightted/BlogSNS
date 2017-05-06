73acc6799c8ab8e2151663f00fafa09508c203e1
Mightted <sosowolf0125@gmail.com>
2017-05-06 21:36:04 +0800

    修复部分bug

---
+++ app/src/main/java/com/mightted/blogsns/Utils/SearchItemAdapter.java
@@ -0,0 +1,10 @@
+package com.mightted.blogsns.Utils;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by 晓深 on 2017/4/27.
+ */
+
+public class SearchItemAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/mightted/blogsns/domain/entity/SearchItems.java
@@ -0,0 +1,8 @@
+package com.mightted.blogsns.domain.entity;
+
+/**
+ * Created by 晓深 on 2017/4/27.
+ */
+
+public class SearchItems {
+}
